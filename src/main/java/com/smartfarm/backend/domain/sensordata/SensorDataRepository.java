b1bc34537ee19a64c1930acbc8affc61f511b52c
ajy9851 <ajy4304@naver.com>
2025-03-26 18:43:48 +0900

    feat: 가장 최근 저장된 센서 데이터 조회 기능 추가

--- src/main/java/com/smartfarm/backend/domain/sensordata/SensorDataRepository.java
+++ src/main/java/com/smartfarm/backend/domain/sensordata/SensorDataRepository.java
@@ -1,6 +1,8 @@
 package com.smartfarm.backend.domain.sensordata;
 
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface SensorDataRepository extends MongoRepository<SensorData, String> {
+    Optional<SensorData> findTopByOrderByTimestampDesc();
 }
